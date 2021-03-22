e443990c72817f4d5bc359b2a596870df72fcae8
Gustavo Cardoso <gustavocardoso@gmail.com>
2021-03-21 19:03:08 -0700

    Changing fallback to blocking

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -11,7 +11,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   const paths = posts.map(post => `/posts/${post.id}`)
 
-  return { paths, fallback: false }
+  return { paths, fallback: 'blocking' }
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
